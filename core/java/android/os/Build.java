3cd6b54ae897597aceb8f678349903da04d2db8f
Christian Oder <myself5@carbonrom.org>
2018-05-12 15:01:23 +0200

    fwb: Add check for odm version
    We update system more often than ODM. This allows linking the ODM image version to system.
    
    Test: flash outdated odm image on device with system defined odm revision and expect error
    
    Change-Id: Ifab6ca5c2f97840bb4192226f191e624267edb32

--- core/java/android/os/Build.java
+++ core/java/android/os/Build.java
@@ -1084,6 +1084,8 @@ public class Build {
 
         final String system = SystemProperties.get("ro.system.build.fingerprint");
         final String vendor = SystemProperties.get("ro.vendor.build.fingerprint");
+        final String odm_expected = SystemProperties.get("ro.odm.expect.version");
+        final String odm = SystemProperties.get("ro.odm.version");
         final String bootimage = SystemProperties.get("ro.bootimage.build.fingerprint");
         final String requiredBootloader = SystemProperties.get("ro.build.expect.bootloader");
         final String currentBootloader = SystemProperties.get("ro.bootloader");
@@ -1099,6 +1101,14 @@ public class Build {
             if (!Objects.equals(system, vendor)) {
                 Slog.e(TAG, "Mismatched fingerprints; system reported " + system
                         + " but vendor reported " + vendor);
+                return false;
+            }
+        }
+
+        if (!TextUtils.isEmpty(odm_expected)) {
+            if (!Objects.equals(odm_expected, odm)) {
+                Slog.e(TAG, "Mismatched fingerprints; system reported " + odm_expected
+                        + " but odm reported " + odm);
                 return false;
             }
         }
